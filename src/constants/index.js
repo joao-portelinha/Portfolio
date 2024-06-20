9948071f322a1cf34ac33236a62dd7aca7020201
João Portelinha <joaoportelinha@hotmail.com>
2024-06-20 01:07:12 +0100

    Initial Commit

---
+++ src/constants/index.js
@@ -0,0 +1,61 @@
+import project1 from "../assets/projects/project-1.jpg";
+
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "March 2018 - August 2018",
+    role: "IT Technician - Internship",
+    company: "DO IT BETTER!",
+    description: ``,
+    technologies: ["Hardware", "HTML", "CSS", "Javascript"],
+  },
+  {
+    year: "May 2016 - July 2016",
+    role: "Frontend Developer",
+    company: "Adobe",
+    description: ``,
+    technologies: ["HTML", "CSS", "Javascript"],
+  },
+];
+
+export const EDUCATION = [
+  {
+    year: "2019 - 2024",
+    degree: "Computer Science - Bachelor's Degree",
+    school: "Escola Superior de Tecnologia e Gestão (ESTIG) - Beja",
+    description: `Bachelor's Degree in Computer Science which corresponds to the level 6 QRQ.`,
+    technologies: ["Java", "Python", "C", "SQL", "HTML", "CSS", "Javascript", "Laravel", "React", ".NET", "Kotlin", "SysAdmin"],
+  },
+  {
+    year: "2016 - 2018",
+    degree: "Management Informatics - Higher Professional Technical Course",
+    company: "Instituto Superior de Tecnologias Avançadas - Lisboa",
+    description: `A two-year Higher Professional Technical Course in Management Informatics where I was able to deepen my knowledge of IT and business management. This course corresponds to the level 5 QRQ.`,
+    technologies: ["HTML", "CSS", "Javascript", "Primavera", "PHC"],
+  },
+  {
+    year: "2013 - 2016",
+    degree: "Management Informatics - Professional Course",
+    school: "Escola Tecnológica do Litoral Alentejano - Sines",
+    description: ``,
+    technologies: ["VisualBasic", "C", "HTML", "CSS", "Javascript"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Final Graduate Project - Completion of the Passport Process Management System for the Beja District Archive",
+    image: project1,
+    description:
+      "Final project carried out as part of the Computer Engineering degree. This project was a collaboration between IPBeja and the Beja District Archive, with the aim of creating a web page to facilitate document management of passport files and access to them by family members.",
+    technologies: ["Laravel", "PHP", "SQL"],
+  },
+];
+
+export const CONTACT = {
+  phoneNo: "+351 969 070 168",
+  email: "joaoportelinha@hotmail.com",
+};

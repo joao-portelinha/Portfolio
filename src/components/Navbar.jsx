9ef18059b89a73e656c48b2395e0d4b519ac32fa
João Portelinha <joaoportelinha@hotmail.com>
2024-06-21 16:22:18 +0100

    Hero Section + Logo Removed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,6 @@ const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
         <div className="flex flex-shrink-0 items-center">
-            <img className="mx-2 w-10" src={logo} alt="logo"></img>
         </div>
         <div className="m-8 flex items-center justify-center gap-4 text-2xl">
             <FaLinkedin />
